db63aea22560532347084c9396bad116b2321550
Ryan Ramage <ryan.ramage@gmail.com>
2018-02-03 13:24:29 -0700

    cleanup a bit

--- src/JoiEditor.vue
+++ src/JoiEditor.vue
@@ -32,20 +32,13 @@
       }
     },
     mounted () {
-
       let info = {
         schema: this.schema._inner,
         initialValue: this.initialValue,
         errors: {}
       }
-
       this.$set(this.$data, 'joiObject', info)
     },
-
-    beforeDestroy () {
-      console.log('beforeDestroy')
-    },
-
     methods: {
       save () {
         this.schema.validate(this.initialValue, {abortEarly: false}, (err, result) => {
@@ -64,29 +57,6 @@
     }
   }
 </script>
-
-
 <style>
-  .VideoBg {
-    position: relative;
-    background-size: cover;
-    background-position: center;
-    overflow: hidden;
-  }
 
-  .VideoBg video {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    visibility: hidden;
-    transform: translate(-50%, -50%);
-  }
-
-  .VideoBg__content {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
 </style>
